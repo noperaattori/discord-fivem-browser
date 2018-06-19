6fd38a881d85bec5ffec8629212cf2605ad91a2f
Noperaattori <noperaattori@pm.me>
2018-06-19 15:45:54 +0300

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports.options = {
+    token: '',
+    topicChannels: [
+      'general'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'Server1', url: 'http://127.0.0.1:30120', alias: 'NewYork' },
+        { name: 'Server2', url: 'http://127.0.0.1:30121', alias: 'LosAngeles' }
+    ],
+    pollRate: 30,
+};
---
+++ main.js
@@ -0,0 +1,99 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+const options = require('./config/config').options;
+const _ = require('lodash');
+const client = new Discord.Client();
+
+/**
+ * Get the players.json
+ */
+const getPlayers = async server => {
+  try {
+    return axios.get(`${server}/players.json`, { responseType: 'json', timeout: 10000 });
+  } catch(e) {
+    console.log(e);
+  }
+};
+
+/**
+ * Connect to Discord
+ */
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!\nIf this is your first time connecting, use https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=67176464&scope=bot to authorize your bot for your server.`);
+});
+
+/**
+ * Poll the servers every X seconds
+ */
+client.setInterval(async () => {
+  
+  // Populate the servers array with data from players.json
+  const servers = [];
+  let total = 0;
+  for (const server of options.servers) {
+    let players = await getPlayers(`${server.url}/players.json`);
+    servers.push({
+      name: _.capitalize(server.name),
+      count: players.data.length,
+    });
+    total =+ players.data.length;
+  };
+  
+  // Generate the topic string
+  const counts = [];
+  for (const value of servers) {
+    counts.push(`${value.name}: ${value.count}`);
+  }
+  counts.push(`Total: ${total}`);
+  const topic = counts.join(' | ');
+
+  // Set topics for set channels
+  for (const channel of options.topicChannels){
+    try {
+      //console.log(topic);
+      client.channels.find('name', channel).setTopic(topic);
+    } catch(e){
+      console.log(e);
+    }
+  }
+}, options.pollRate * 1000);
+
+/**
+ * ![server name] commands, messages the player list for the current channel
+ */
+client.on('message', message => {
+
+  // Ignore other than legit commands
+  if(message.channel.type !== 'text' || message.member === null || !message.content.startsWith('!')) {
+    return false;
+  }
+
+  // Check if the messager has the required role
+  let foundRole = false;
+  for (const role of options.roles) {
+    if (message.member.roles.find("name", role)){
+      foundRole = true;
+      break;
+    }
+  }
+  if (!foundRole) {
+    return false;
+  }
+
+  // Parse the command and find a matching server from options array
+  const rawCommand = String(message.content).substr(1).trim();
+  const server = options.servers.find(k => k.name === rawCommand || k.alias === rawCommand);
+
+  getPlayers(server.url).then(response => {
+    let block = [_.capitalize(server.name) + " (" + response.data.length + "/32)", "---"];
+    for (let player of response.data) {
+      let playerId = _.padStart(player.id, 2);
+      block.push("[" + playerId + "] " + player.name + " : " + player.identifiers[0]);
+    }
+    message.channel.send(block.concat("\n"), {code: true});
+  }).catch(error => { console.log(error) });
+  }
+);
+
+// Log in
+client.login(options.token);
